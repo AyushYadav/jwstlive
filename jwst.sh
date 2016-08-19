35ae2b4267529b27a4854133e9d1842cc3556bb1
mmaclay <mtmaclay@gmail.com>
2016-08-19 11:56:42 -0400

    $HOME add

--- jwst.sh
+++ jwst.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-rm /Users/mmaclay/jwst_im/img.jpg
-rm /Users/mmaclay/jwst_im/img1.jpg
+rm $HOME/jwst_im/img.jpg
+rm $HOME/jwst_im/img1.jpg
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/jwst_im/webb/webb.jpg"'
 sleep 20s
 for i in `seq 1 100`;
     do
 		curl "http://www.jwst.nasa.gov/WebbCamWide/CLNRMR-800px.jpg?1471471577817" > /Users/mmaclay/jwst_im/img.jpg
 		osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/jwst_im/img.jpg"'
-		rm /Users/mmaclay/jwst_im/img1.jpg
+		rm $HOME/jwst_im/img1.jpg
 		sleep 62s      
-		curl "http://www.jwst.nasa.gov/WebbCamWide/CLNRMR-800px.jpg?1471471577817" > /Users/mmaclay/jwst_im/img1.jpg       
+		curl "http://www.jwst.nasa.gov/WebbCamWide/CLNRMR-800px.jpg?1471471577817" > $HOME/jwst_im/img1.jpg       
 		osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/jwst_im/img1.jpg"'
-		rm /Users/mmaclay/jwst_im/img.jpg
+		rm $HOME/jwst_im/img.jpg
 		sleep 62s    
     done
