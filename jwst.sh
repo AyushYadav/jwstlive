d9ddef88400d5dafb6988d10c6556e2d3b1811e7
mmaclay <mtmaclay@gmail.com>
2016-08-21 13:30:09 -0500

    not generalized, but working

--- jwst.sh
+++ jwst.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-rm /Users/mmaclay/jwst_im/img.jpg
-rm /Users/mmaclay/jwst_im/img1.jpg
+rm $HOME/jwst_im/img.jpg
+rm $HOME/jwst_im/img1.jpg
 export live_pid = $!
-./Users/mmaclay/jwst_im/terminate.sh
+$HOME/jwst_im/terminate.sh
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/jwst_im/webb/webb.jpg"'
-sleep 20s
+sleep 5s
 for i in `seq 1 2`;
     do
 		curl "http://www.jwst.nasa.gov/WebbCamWide/CLNRMR.jpg?1471632928081" > $HOME/jwst_im/img.jpg
-		osascript -e 'tell application "Finder" to set desktop picture to POSIX file "$HOME/jwst_im/img.jpg"'
+		osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/jwst_im/img.jpg"'
 		rm $HOME/jwst_im/img1.jpg
 		sleep 62s      
 		curl "http://www.jwst.nasa.gov/WebbCamWide/CLNRMR.jpg?1471632928081" > $HOME/jwst_im/img1.jpg       
-		osascript -e 'tell application "Finder" to set desktop picture to POSIX file "$HOME/jwst_im/img1.jpg"'
-		rm /Users/mmaclay/jwst_im/img.jpg
+		osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/jwst_im/img1.jpg"'
+		rm $HOME/jwst_im/img.jpg
 		sleep 62s    
     done
--- terminate
+++ terminate
@@ -1,3 +1,5 @@
 #!/bin/bash
 kill $live_pid
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "$HOME/Pictures/west2.jpg"'
+#IM = 'Users/mmaclay/jwst_im/webb/webb.jpg'
+#echo $IM
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/mmaclay/Pictures/west2.jpg"'
